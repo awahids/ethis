a7533740cfe0ca44fb01bca0419bbda16359c711
awahids <awahid.safhadi@gmail.com>
2022-08-12 18:56:23 +0800

    add model and table news

--- app/Models/news.php
+++ app/Models/news.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class news extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title', 'body', 'images', 'status'
+    ];
 }
--- database/migrations/2022_08_12_085851_create_news_table.php
+++ database/migrations/2022_08_12_085851_create_news_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->json('images')->nullable();
+            $table->enum('status', ['deleted', 'published', 'draft']);
             $table->timestamps();
         });
     }
