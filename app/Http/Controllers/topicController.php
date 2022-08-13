b0af66fe6ee95e28d87038d93864ca90458d3cb3
awahids <awahid.safhadi@gmail.com>
2022-08-13 19:18:14 +0800

    fix  name of controllers

--- app/Http/Controllers/topicController.php
+++ app/Http/Controllers/topicController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class topicController extends Controller
+class TopicController extends Controller
 {
     /**
      * Display a listing of the resource.
